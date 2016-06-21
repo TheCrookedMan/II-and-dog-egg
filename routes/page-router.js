f021f5f24216ea8bc0eb29c821593c193aa6330c
TheCrookedMan <thecrookedman@163.com>
2016-06-21 11:43:43 +0800

    增加

--- routes/page-router.js
+++ routes/page-router.js
@@ -1,4 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
+router.get('/index/index.html', (req, res, next) => {
+    return res.render('index/index', { title: '首页' });
+});
+
 module.exports = router;
