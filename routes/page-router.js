41596b6b75daea992a216bd55f87a93ee660d634
TheCrookedMan <thecrookedman@163.com>
2016-06-29 11:07:40 +0800

    的

--- routes/page-router.js
+++ routes/page-router.js
@@ -5,6 +5,10 @@ router.get('/index/index.html', (req, res, next) => {
     return res.render('index/index', { title: '首页' });
 });
 
+router.get('/package/package.html', (req, res, next) => {
+    return res.render('package/package', { title: '套餐二级页' });
+});
+
 router.get('*', (req, res, next) => {
     return res.render('index', { title: '首页' });
 });
