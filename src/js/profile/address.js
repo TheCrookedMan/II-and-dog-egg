dc09ef0987f21714d3d3c9215ac8011268606ccb
TheCrookedMan <thecrookedman@163.com>
2016-07-07 17:35:32 +0800

    接口

--- src/js/profile/address.js
+++ src/js/profile/address.js
@@ -1,5 +1,12 @@
-$(function(){
-	$('.set-default').on("click",function(){
-　　　　$(this).toggleClass("cur")
-　　});
-})
+(function() {
+	$.get('/template/profile/profile_address.t').success(function(data) {
+        //data = data.replace(/(^\s+)|(\s+$)/g, "");
+        if ("" !== data) {
+            $(".title").append(data);
+        }
+    }).error(function(err) {});
+
+// 	$('.set-default').on("click",function(){
+// 　　　　$(this).toggleClass("cur")
+// 　　});
+}).call(this)
