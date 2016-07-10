05fcfddd20fcc55dc7ed033be4cb16f08953f29e
theCrookedMan@163.com <theCrookedMan@163.com>
2016-07-10 16:30:19 +0800

    提交修改..

--- src/js/basket/basket.js
+++ src/js/basket/basket.js
@@ -1,22 +1,25 @@
 (function() {
 
-//     $('.list-li').touchWipe({itemDelete: '.btn'});
-$.get('/template/basket/basket_list.t',{"uid":9}).success(function(data) {
+    
+    $.get('/template/basket/basket_list.t', { "uid": 9 }).success(function(data) {
         data = data.replace(/(^\s+)|(\s+$)/g, "");
         if ("" !== data) {
-        	$(".pub_noData").hide();
+            $(".pub_noData").hide();
             $(".list-ul").html(data);
-            if($(".list-ul li.can").length>0){
+            if ($(".list-ul li.can").length > 0) {
 
-            }
-            else{
-            	
+            } else {
+
             }
         }
 
-        $('.set-default').on("click",function(){
-	        $(this).toggleClass("cur");
-	        $("#total").toggleClass("active");
-	　　});
+        $('.set-default').on("click", function() {
+            $(this).toggleClass("cur");
+            $("#total").toggleClass("active");　　
+        });
+
+        $('.list-li').touchwipe({ itemDelete: '.btn' });
     }).error(function(err) {});
+
+   
 }).call(this)
