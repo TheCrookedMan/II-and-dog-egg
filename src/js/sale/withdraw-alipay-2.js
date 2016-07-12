73f6f59832c066665a98966005a46db254315072
theCrookedMan@163.com <theCrookedMan@163.com>
2016-07-12 11:09:19 +0800

    提交代码...

--- src/js/sale/withdraw-alipay-2.js
+++ src/js/sale/withdraw-alipay-2.js
@@ -1,3 +1,48 @@
-(function(){
-	
+(function() {
+    $("#alipay-password").on("keydown", "#password", function(ev) {
+        if (ev.keyCode >= 48 && ev.keyCode <= 57) {
+            var number = $(this).val();
+            var len = number.length + 1;
+            if (len > 6) {
+                $(this).blur();
+            } else {
+                $.each($(".password-panel span"), function(i, I) {
+                    if (i < len) {
+                        $(I).text("*");
+                    } else {
+                        $(I).text("");
+                    }
+                });
+                if (len == 6) {
+                    $(this).blur();
+                    setTimeout(function() {
+                        $("#alipay-password").modal('toggle');
+                    }, 500)
+                    submitPassword();
+                }
+            }
+        } else if (ev.keyCode == 8) {
+            var number = $(this).val();
+            var len = number.length - 1;
+            $.each($(".password-panel span"), function(i, I) {
+                if (i < len) {
+                    $(I).text("*");
+                } else {
+                    $(I).text("");
+                }
+            });
+        }
+
+    });
+    $("#alipay-password").on("open.modal.amui", function() {
+        var list = $(this).find(".password-panel span");
+        $.each(list, function(k, K) {
+            $(K).text("");
+        });
+        $("#password").val("");
+    })
+
+    function submitPassword() {
+
+    }
 }).call(this)
