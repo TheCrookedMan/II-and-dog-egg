45faee98ebd52b676e99422084c548064de688ea
TheCrookedMan <thecrookedman@163.com>
2016-07-21 14:33:32 +0800

    提交修改

--- src/js/product/list.js
+++ src/js/product/list.js
@@ -57,7 +57,7 @@
                         $("ul.am-gallery").html("");
                         self.sortcolumn = 3;
                         self.sortdirection = 1;
-                        $("ul.am-gallery").html('<li class="no-data"><p><img src="/img/em3.png"></p><p>二丫家还没有这款商品诶~ <br>您再看看别哒~</p></li>');
+                        $("ul.am-gallery").html('');
                         self.get();
                         scroll.on(function() {
                             if (!self.isEnd) {
@@ -72,7 +72,7 @@
                         $("ul.am-gallery").html("");
                         self.sortcolumn = 3;
                         self.sortdirection = 0;
-                        $("ul.am-gallery").html('<li class="no-data"><p><img src="/img/em3.png"></p><p>二丫家还没有这款商品诶~ <br>您再看看别哒~</p></li>');
+                        $("ul.am-gallery").html('');
                         self.get();
                         scroll.on(function() {
                             if (!self.isEnd) {
@@ -92,7 +92,7 @@
                         $("ul.am-gallery").html("");
                         self.sortcolumn = 2;
                         self.sortdirection = 1;
-                        $("ul.am-gallery").html('<li class="no-data"><p><img src="/img/em3.png"></p><p>二丫家还没有这款商品诶~ <br>您再看看别哒~</p></li>');
+                        $("ul.am-gallery").html('');
                         self.get();
                         scroll.on(function() {
                             if (!self.isEnd) {
@@ -107,7 +107,7 @@
                         $("ul.am-gallery").html("");
                         self.sortcolumn = 2;
                         self.sortdirection = 0;
-                        $("ul.am-gallery").html('<li class="no-data"><p><img src="/img/em3.png"></p><p>二丫家还没有这款商品诶~ <br>您再看看别哒~</p></li>');
+                        $("ul.am-gallery").html('');
                         self.get();
                         scroll.on(function() {
                             if (!self.isEnd) {
@@ -127,7 +127,7 @@
                         $("ul.am-gallery").html("");
                         self.sortcolumn = 0;
                         self.sortdirection = 1;
-                        $("ul.am-gallery").html('<li class="no-data"><p><img src="/img/em3.png"></p><p>二丫家还没有这款商品诶~ <br>您再看看别哒~</p></li>');
+                        $("ul.am-gallery").html('');
                         self.get();
                         scroll.on(function() {
                             if (!self.isEnd) {
@@ -142,7 +142,7 @@
                         $("ul.am-gallery").html("");
                         self.sortcolumn = 0;
                         self.sortdirection = 0;
-                        $("ul.am-gallery").html('<li class="no-data"><p><img src="/img/em3.png"></p><p>二丫家还没有这款商品诶~ <br>您再看看别哒~</p></li>');
+                        $("ul.am-gallery").html('');
                         self.get();
                         scroll.on(function() {
                             if (!self.isEnd) {
@@ -168,6 +168,7 @@
         },
         get: function() {
             var self = this;
+            modal.loading("open","正在加载...");
             $.get('/template/product/list_gallery.t', {
                 "page": self.pageNo,
                 "pagesize": self.pageSize,
@@ -176,6 +177,7 @@
                 "cid": self.categoryId
             }).success(function(data) {
                 data = data.replace(/(^\s+)|(\s+$)/g, "");
+                modal.loading("close")
                 if ("" == data) {
                     self.isEnd = true;
                 } else {
